7837a5fccf6888d167a232946cb6e28c560fc171
Guillaume Charmetant <cguille.dev@gmail.com>
2017-12-27 21:48:15 +0100

    add dummy iron server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[macro_use] extern crate log;
+extern crate simplelog;
+extern crate iron;
+
+use simplelog::*;
+use iron::prelude::*;
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    info!("hello_world");
+    Ok(Response::with((iron::status::Ok, "Hello World")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    TermLogger::init(LogLevelFilter::Info, Config::default()).unwrap();
+
+    let chain = Chain::new(hello_world);
+    Iron::new(chain).http("localhost:3000").unwrap();
 }
