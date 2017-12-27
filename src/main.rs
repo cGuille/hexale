3ddc98e625977ad17c962fe75748828e0a5b9aa7
Guillaume Charmetant <cguille.dev@gmail.com>
2017-12-27 22:11:49 +0100

    serve static files

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 extern crate simplelog;
+
 extern crate iron;
+extern crate mount;
+extern crate router;
+extern crate staticfile;
 
 use simplelog::*;
 use iron::prelude::*;
 
-fn hello_world(_: &mut Request) -> IronResult<Response> {
-    info!("hello_world");
+use mount::Mount;
+use router::Router;
+use staticfile::Static;
+
+use std::path::Path;
+
+fn hello_world(request: &mut Request) -> IronResult<Response> {
+    info!("Running hello_world handler, URL path: {}", request.url.path().join("/"));
     Ok(Response::with((iron::status::Ok, "Hello World")))
 }
 
 fn main() {
     TermLogger::init(LogLevelFilter::Info, Config::default()).unwrap();
 
-    let chain = Chain::new(hello_world);
-    Iron::new(chain).http("localhost:3000").unwrap();
+    let mut router = Router::new();
+    router.get("/hello", hello_world, "hello");
+
+    let mut mount = Mount::new();
+    mount.mount("/", Static::new(Path::new("static")));
+    mount.mount("/api", router);
+
+    Iron::new(mount).http("localhost:3000").unwrap();
 }
