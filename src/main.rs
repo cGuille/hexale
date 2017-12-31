3fb04a3e9f544f9fefe1dc91ac6def009a704fcd
Guillaume Charmetant <cguille.dev@gmail.com>
2017-12-31 15:45:46 +0100

    add string normalization function
    We will need to compare the word chosen by one player with the guess
    provided by another player. Players might not write the same word the
    same way but the words must still be considered equal. For instance, if
    player1 chose "accusé" and player2 guesses "ACCUSE", it should be a
    match.
    
    In order to do so, we introduce a normalization function which transform
    strings into a normalized form. We will then compare the normalized
    forms instead of the original strings.

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,26 @@ extern crate iron;
 extern crate mount;
 extern crate router;
 extern crate staticfile;
+extern crate unicode_normalization;
 
-use simplelog::*;
 use iron::prelude::*;
-
 use mount::Mount;
 use router::Router;
+use simplelog::*;
 use staticfile::Static;
-
+use std::ascii::AsciiExt;
 use std::path::Path;
+use unicode_normalization::UnicodeNormalization;
 
 fn hello_world(request: &mut Request) -> IronResult<Response> {
     info!("Running hello_world handler, URL path: {}", request.url.path().join("/"));
-    Ok(Response::with((iron::status::Ok, "Hello World")))
+    Ok(Response::with((iron::status::Ok, normalize(String::from("Hello World")))))
+}
+
+// This function can be used to check guesses: instead of comparing "accusé"
+// with "ACCUSE", compare normalize("accusé") with normalize("ACCUSE").
+fn normalize(input: String) -> String {
+    input.to_lowercase().nfkd().filter(|char| char.is_ascii()).collect()
 }
 
 fn main() {
